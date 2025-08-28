dffcc98aad4ad21c46587c868976f49ed10d11de
SkippyPixie <veronicajefferies@gmail.com>
2025-08-28 10:20:34 -0400

    chore: initial scaffold (index.html + sw.js)

---
+++ sw.js
@@ -0,0 +1,14 @@
+const SHELL='dc-shell-scaffold';
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(SHELL).then(c=>c.addAll(['/', '/index.html'])));
+});
+self.addEventListener('activate', e=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e=>{
+  const r=e.request, u=new URL(r.url);
+  if(r.method!=='GET') return;
+  if(u.origin===location.origin){
+    e.respondWith(caches.match(r).then(m=>m||fetch(r).then(resp=>{
+      caches.open(SHELL).then(c=>c.put(r,resp.clone())); return resp;
+    })));
+  }
+});
