5d01afb1a3965c775fbf473212434bcb76736675
SkippyPixie <veronicajefferies@gmail.com>
2025-09-07 19:52:33 -0400

    feat: expand service worker assets and cleanup

--- sw.js
+++ sw.js
@@ -4,12 +4,23 @@ const DATA  = 'dc-data-v1';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(SHELL).then(c => c.addAll(['/', '/index.html']))
+    caches.open(SHELL).then(c => c.addAll([
+      '/',
+      '/index.html',
+      '/styles.css',
+      '/sw.js'
+    ]))
   );
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys
+        .filter(k => k.startsWith('dc-') && ![SHELL, DATA].includes(k))
+        .map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
